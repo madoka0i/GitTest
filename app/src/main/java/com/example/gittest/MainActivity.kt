f198861fedf05f3f77bb4695c18103205f9559cd
madoka0i <mmmmadoka@gmail.com>
2020-02-24 23:52:41 +0900

    v1 (releaseからmasterにマージ)

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("debug", "onCreate()")
+        Log.d("debug", "あああああ")
+        Log.d("debug", "feature/p1 変更3")
     }
+
+    // feature/p2ブランチ作成
+
+    // developブランチ作成
+
+    // feature/p1ブランチ作成
 }
